d4005772a2f4dca961f60d014e39c1badb49b4ad
Михайленко Максим <mihailenkomaks2005@gmail.com>
2024-03-20 15:15:37 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+def calculate_rectangle_area(width, length):
+    area = width * length
+    return area
+
+
+width1 = 20
+length1 = 30
+area1 = calculate_rectangle_area(width1, length1)
+print(f"Площадь прямоугольника с шириной {width1} и длиной {length1} равна {area1}")
+
+width2 = 45
+length2 = 77
+area2 = calculate_rectangle_area(width2, length2)
+print(f"Площадь прямоугольника с шириной {width2} и длиной {length2} равна {area2}")
---
+++ 4.py
@@ -0,0 +1,10 @@
+def find_max(numbers):
+    max_number = max(numbers)
+    print("Максимальное число из списка:", max_number)
+
+
+list1 = [2, 50, 3, 9, 12]
+find_max(list1)
+
+list2 = [88, 33, 30, 56, 49]
+find_max(list2)
